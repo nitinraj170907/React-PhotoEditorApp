757432186a186fcb41a9cb91d67da2163794f033
NITIN RAJ <nitinraj170907@gmail.com>
2022-05-07 22:15:37 +0530

    Updates

---
+++ precache-manifest.5bfeb1d986040d779f5338f8683dc612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a2d053913c81933a3e878d6f57be75",
+    "url": "/nitinraj170907/React-PhotoEditorApp/index.html"
+  },
+  {
+    "revision": "9c0f76c8fb6137d40e3c",
+    "url": "/nitinraj170907/React-PhotoEditorApp/static/css/main.2e544694.chunk.css"
+  },
+  {
+    "revision": "dbb6f9bcb49dd7a583d8",
+    "url": "/nitinraj170907/React-PhotoEditorApp/static/js/2.0103e952.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nitinraj170907/React-PhotoEditorApp/static/js/2.0103e952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c0f76c8fb6137d40e3c",
+    "url": "/nitinraj170907/React-PhotoEditorApp/static/js/main.792e336a.chunk.js"
+  },
+  {
+    "revision": "0b03035c26ff6748a408",
+    "url": "/nitinraj170907/React-PhotoEditorApp/static/js/runtime-main.566a07cc.js"
+  }
+]);
